f8af25ccc47c9bcbdf601069573553718e6f0d12
smou14 <136638687+smou14@users.noreply.github.com>
2026-08-01 11:59:44 +0100

    First UI button test
    does nothing but testing the display

---
+++ UI.py
@@ -0,0 +1,32 @@
+from customtkinter import *
+UI = CTk()
+UI.geometry("1280x720")
+
+# START Define main menu btns
+IR_btn = CTkButton(master=UI,  text="IR",fg_color="#000000",
+hover_color="#090404",text_color="#ff2775",
+font=("aerial",100), width=300, height=150,
+border_color="#008cff", border_width=7)
+
+NFC_btn = CTkButton(master=UI,  text="NFC",fg_color="#000000",
+hover_color="#090404",text_color="#5200ff",
+font=("aerial",100), width=300, height=150,
+border_color="#008cff", border_width=7)
+
+RFID_btn = CTkButton(master=UI,  text="RFID",fg_color="#000000",
+hover_color="#090404",text_color="#27ff84",
+font=("aerial",100), width=300, height=150,
+border_color="#008cff", border_width=7)
+
+SDR_btn = CTkButton(master=UI,  text="SDR",fg_color="#000000",
+hover_color="#090404",text_color="#ffffff",
+font=("aerial",100), width=300, height=150,
+border_color="#008cff", border_width=7)
+# END Define main menu btns
+# START btn places
+IR_btn.place(relx=(5/16),rely=0.5,anchor="center")
+NFC_btn.place(relx=(9/16),rely=0.5,anchor="center")
+RFID_btn.place(relx=(7/16),rely=0.75,anchor="center")
+SDR_btn.place(relx=(11/16),rely=0.75,anchor="center")
+# END btn places
+UI.mainloop()
