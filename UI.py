9ab74cf6334eefd054368c168f410e235347230c
smou14 <136638687+smou14@users.noreply.github.com>
2026-08-05 09:20:04 +0100

    IR-tx code entry

--- UI.py
+++ UI.py
@@ -4,8 +4,8 @@ import subprocess
 set_appearance_mode("dark")
 UI = CTk()
 UI.geometry("800x480")
-#UI.attributes("-fullscreen", True)   # fullscreen
-#UI.overrideredirect(True)
+UI.attributes("-fullscreen", True)   # fullscreen
+UI.overrideredirect(True)
 current_frame = None
 font="DejaVu Sans Mono"
 # START btn funcs
