f8c95b55304cc01d37c278a8a91e7b076d25458e
smou14 <136638687+smou14@users.noreply.github.com>
2026-08-06 16:08:31 +0100

    WiFi updater

--- UI.py
+++ UI.py
@@ -203,8 +203,8 @@ border_color="#008cff", border_width=7,command=lambda: show_frame(mainMenu))
 EXIT_WIFI_btn.place(relx=(15/16),rely=0.125,anchor="center")
 
 WIFI_box = CTkTextbox(master=WIFIMenu, fg_color="#000000" ,text_color="#33ddff",
-border_color="#33ddff", border_width=7, width=800, height=300)
-WIFI_box.place(relx=(8/16),rely=0.6,anchor="center")
+border_color="#33ddff", border_width=7, width=600, height=300)
+WIFI_box.place(relx=(4/16),rely=0.6,anchor="center")
 WIFI_box.insert("0.0", getWifiData())
 
 
