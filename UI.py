9bb04bdd74172e15447b82d53082fc8d08d216a1
smou14 <136638687+smou14@users.noreply.github.com>
2026-08-08 15:58:31 +0100

    Loading txt

--- UI.py
+++ UI.py
@@ -68,6 +68,8 @@ def getWifiData():
         print(result.stderr)
 def showWifiData():
     WIFI_box.delete("1.0", "end")
+    WIFI_box.insert("0.0", "Loading ... ")
+    WIFI_box.delete("1.0", "end")
     WIFI_box.insert("0.0", getWifiData())
 def nfc():
     print("NFC")
